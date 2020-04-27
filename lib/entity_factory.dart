6c657a8346caff63f2769c3c9a346aadb546c2de
lchjczw <lchjczw@163.com>
2020-04-27 12:17:04 +0800

    fix

--- lib/entity_factory.dart
+++ lib/entity_factory.dart
@@ -1,7 +1,7 @@
 //import '../login/models/login_entity.dart';
 //import '../order/models/search_entity.dart';
 //import '../user/models/user_entity.dart';
-
+//
 //import 'order/models/search_entity.dart';
 // todo 修改为接口实现
 class EntityFactory {
@@ -10,10 +10,10 @@ class EntityFactory {
       return null;
 //    } else if (T.toString() == 'SearchEntity') {
 //      return SearchEntity.fromJson(json) as T;
-    } else if (T.toString() == 'UserEntity') {
-      return UserEntity.fromJson(json) as T;
-    } else if (T.toString() == 'LoginEntity') {
-      return LoginEntity.fromJson(json) as T;
+//    } else if (T.toString() == 'UserEntity') {
+//      return UserEntity.fromJson(json) as T;
+//    } else if (T.toString() == 'LoginEntity') {
+//      return LoginEntity.fromJson(json) as T;
     } else {
       return null;
     }
